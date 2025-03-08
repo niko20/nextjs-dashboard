b67a084b4a5029ad9ab5de8fcc7cac6ee36b9fb1
Nico Figueroa <figueroanicolas.a@gmail.com>
2025-03-08 12:07:36 -0300

    feat: debounce

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: "incremental",
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
